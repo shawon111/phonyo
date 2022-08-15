a042e9884dde69eb071eacdff6cd18f364daf868
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-08-16 03:40:55 +0600

    complete

---
+++ phonyo-ecommerce/schemas/product.js
@@ -0,0 +1,40 @@
+export default {
+    name: "product",
+    title: "Product",
+    type: "document",
+    fields: [
+        {
+            name: "image",
+            title: "Image",
+            type: "array",
+            of: [{type: "image"}],
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: "title",
+            title: "Product Title",
+            type: "string"
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "title",
+                maxLength: 90
+            }
+        },
+        {
+            name: "price",
+            title: "Price",
+            type: "number"
+        },
+        {
+            name: "details",
+            title: "Details",
+            type: "string"
+        }
+    ]
+}
